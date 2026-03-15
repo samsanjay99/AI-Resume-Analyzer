33f20a0ce504748093538ae3397186a26a7b7f7d
samsanjay99 <sanjaykumarcp9900@gmail.com>
2026-03-15 09:52:59 +0530

    Add config/__init__.py and fix utils/__init__.py safe import

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,5 +6,8 @@ from .resume_analyzer import ResumeAnalyzer
 from .resume_builder import ResumeBuilder
 from .resume_parser import ResumeParser
 from .excel_manager import ExcelManager
-from .database import * 
+try:
+    from .database import *
+except Exception:
+    pass
 from .ai_resume_analyzer import AIResumeAnalyzer

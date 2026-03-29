35231df153be840622735779fe8957036a2adf03
samsanjay99 <sanjaykumarcp9900@gmail.com>
2026-03-29 13:21:53 +0530

    Add interview page for GitHub Pages

--- jobs/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Job Search Module
-Provides job search functionality across multiple job portals
-"""
-
-from .job_search import render_job_search
-from .job_portals import JobPortal
-
-__all__ = ['render_job_search', 'JobPortal']
